1134b5ef2822217c9763e33d3480a4911ba532a5
Evelyn Law <evelynlawyeeting@gmail.com>
2019-02-16 23:54:02 -0500

    Updated JS files

--- Rainbow JS.js
+++ Rainbow JS.js
@@ -4,10 +4,11 @@ var guessColours = new Array(5);
 var index = 0;
 var lvlsPassed = 0;
 
-// need to fill in colours for the bubbles!!!!! HTML DOC MUST CHANGE
+// need to fill in colours for the bubbles!!!!! HTML DOC MUST CHANGE  
 
 // Resets colour scheme of game
 function resetColour() {
+	var colour_list = document.getElementsByClassName("").innerHTML;
 	if (lvlsPassed <= 5) {
 		colours = {#ffcccc, #ffe6cc, #ffffcc, #e6ffcc, #ccffcc, #ccffff, #cce6ff, #ccccff, #e6ccff, #ffcce6};
 	}
@@ -26,15 +27,64 @@ function resetColour() {
 	else {
 		colours = {#f2ffe6, #e5ffcc, #d3ffad, #beff84, #adff66, #ffffe6, #ffffcc, #ffffac, #ffff8e, #ffff66};
 	}
+	for (i = 0; i <= 9; ++i) {
+		colour_list[i].style.background-color = colours[i];
+		setTimeout(hideQuestion, 5000);
+	}
+}
+
+// Hides the question and reveals the guess page
+function hideQuestion() {
+	var toHide = document.getElementsByClassName("question").innerHTML;
+	toHide.style.display = "hidden";
+	var toShow = document.getElementByClassName("answer").innerHTML;
+	toShow.style.display = "inline"; 
+}
+
+function transition() {
+	var answer = document.getElementByClassName("answer").innerHTML;
+	// if answer is displayed rn
+	if (answer.style.display === "inline") {
+		whitePage();
+		var toShow = document.getElementByClassName("question").innerHTML;
+	}
+	// if question is displayed rn
+	else {
+		setTimeout(whitePage, 5000);
+		var toShow = document.getElementByClassName("answer").innerHTML;
+	}
+	toShow.style.display = "inline"; 
+}
+
+function whitePage() {
+	var answer = document.getElementByClassName("answer").innerHTML;
+	// hide answer if shown
+	if (answer.style.display === "inline") {
+		answer.style.display = "hidden";
+	}
+	var whiteScreen = document.getElementByClassName("empty").innerHTML;
+	whiteScreen.style.display = "inline";
+	if (answer.style.display === "hidden") {
+		answer.style.display = "inline";
+	}
+}
+
+function hideAnswer() {
+	var toHide = document.getElementsByClassName("answer").innerHTML;
+	toHide.style.display = "inline"; 
+	var toShow = document.getElementByClassName("question").innerHTML;
+	toShow.style.display = "hidden"; 
 }
 
 // Main Program
 // Refreshes the question colours and creates an array for them
 function main() {
 	resetColour();
+	var quest_colour = document.getElementsByClassName("question").innerHTML;
 	for (i = 0; i < 5; ++i) {
 		var randInt = Math.floor(Math.random() * 10);
-		questionColours[i] = colours[randInt];  
+		questionColours[i] = colours[randInt];
+		quest_colour[i].style.background-color = questionColours[i];
 	}
 }
 
@@ -58,5 +108,6 @@ function guessAnswer() {
 	}
 	// they succeeded if this happens! 
 	++lvlsPassed;
+	hideAnswer();
 }
 
